39be74089ec9e1e7384acb6c9ba5f9dd827ade4a
Blair Caple <blair@kaaterskil.com>
2014-07-11 05:09:11 -0400

    Refactor results queries, add groupdate gem

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
-# Represents a registered user (dieter) in the application
+require 'groupdate'
+
 class User < ActiveRecord::Base
   class UserNotAvailableException < RuntimeError; end
   class UserNotTeamLeaderException < RuntimeError; end
@@ -41,11 +42,11 @@ class User < ActiveRecord::Base
   # Creates a new team from the given paramters and assigns the user to the
   # team. An exception is thrown if the new team is invalid.
   #
-  # params - a hash of parameters required to create a team
+  # @param params - a hash of parameters required to create a team
   #
-  # Returns a new Team object that has been instantiated with the given
-  # attributes, linked to this user and that has already been saved
-  # (if it passes the validations).
+  # @eturn A new Team object that has been instantiated with the given
+  #        attributes, linked to this user and that has already been saved
+  #        (if it passes the validations).
   def create_team(params)
     team = managed_teams.create!(params)
     team.users << self
@@ -56,10 +57,10 @@ class User < ActiveRecord::Base
   # and b) if the challenge has not started yet and c) the given user isn't
   # already a team member.
   #
-  # user - the user to add
-  # team - the team to assign to the user
+  # @param user - the user to add
+  # @param team - the team to assign to the user
   #
-  # Returns true on success, false otherwise.
+  # @return True on success, false otherwise.
   def add_user_to_team(team, user)
     # raise UserNotTeamLeaderException if !is_team_leader?(team)
     raise Team::TeamInProgressError if team.is_in_progress?
@@ -74,10 +75,10 @@ class User < ActiveRecord::Base
   # Deletes the row with a primary key matching the id argument, using a
   # SQL DELETE statement.
   #
-  # team - the team from which to remove the given user
-  # user = the user to remove
+  # @param team - the team from which to remove the given user
+  # @param user = the user to remove
   #
-  # Returns the number of rows deleted
+  # @return The number of rows deleted
   def remove_user_from_team(team, user)
     raise UserNotTeamLeaderException if !is_team_leader?(team)
 
@@ -87,9 +88,9 @@ class User < ActiveRecord::Base
   # Starts the given team's challenge if this user is the team leader and
   # the challenge has not already been started.
   #
-  # team - the team to start
+  # @param team - the team to start
   #
-  # Returns true on success, false otherwise
+  # @return True on success, false otherwise
   def start_challenge(team)
     raise UserNotTeamLeaderException if !is_team_leader?(team)
     raise Team::TeamInProgressError if team.is_in_progress?
@@ -105,11 +106,39 @@ class User < ActiveRecord::Base
 
   # Tests if this user is the given team's leader.
   #
-  # team - the team to test its leader
+  # @param team - the team to test its leader
   #
-  # Returns true if this user is the given team's leader, false otherwise.
+  # @return True if this user is the given team's leader, false otherwise.
   def is_team_leader?(team)
     managed_teams.include?(team)
+  end
+
+  # Returns the current active team, or nil if none exists.
+  def fetch_active_team
+    teams.find_by('is_active = ?', true)
+  end
+
+  # Returns a data adjustment representing the differential between the
+  # computed weight of the user at the given weigh-in date and the actual,
+  # in calories.
+  #
+  # @param weigh_in The WeighIn object to assess
+  #
+  # @returns the differential between actual and computer weight, in calories,
+  # as of the end of the day before the weigh-in.
+  def weigh_in(weigh_in)
+    target_intake = rosters.select('target_calories_per_day').
+                            where('team_id = ?', team.id)
+    team = user.fetch_active_team
+    roster = current_user.rosters.where('team_id = ?', team.id)
+
+    starting_weight = roster.starting_weight
+    net_calorie_intake = fetch_team_data(team).map { |k, v|
+       (v - target_intake) if k < weigh_in.entry_date
+    }.reduce(&:+)
+    computed_weight = starting_weight - (net_calorie_intake / 3500)
+
+    (weigh_in.measured_weight - computed_weight) * 3500
   end
 
   # Returns a hash of daily calorie differentials from target, in percent,
@@ -121,32 +150,46 @@ class User < ActiveRecord::Base
     result
   end
 
-  # Returns an array of daily calorie differentials from target, in percent.
-  def fetch_team_data(team)
-    data = []
-    (0..30).each do |i|
-      if !team.starting_on.nil?
-        challenge_date = team.starting_on + i.days
-        data[i] = get_daily_performance(team, challenge_date)
-      else
-        data[i] = 0.00
-      end
-    end
-    data
+  # Returns daily percentage deviation from target calorie intake.
+  #
+  # @param team The team to compute statistics
+  #
+  # @returns An array where keys correspond to the 30 day challenge dates, and
+  # values are the net deviations, in percent.
+  def fetch_team_stats(team)
+    target_intake = rosters.select('target_calories_per_day').
+                            where('team_id = ?', team.id)
+    fetch_team_data.values.map{ |v| (v - target_intake) / target_intake }
   end
 
-  # Returns the calorie differential from target, in percentage terms, for
-  # the given challlenge date
-  def get_daily_performance(team, challenge_date)
-    target = rosters.sum(:target_calories_per_day).
-      where('team_id = ?', team.id)
-    calories_from_meals = meals.join().sum(:total_calories).
-      where('team_id = ? and meal_date = ?', team.id, challenge_date)
-    calories_from_exercise = exercises.sum(:total_calories).
-      where('team_id = ? and entry_date = ?', team.id, challenge_date)
-    calories_from_weigh_ins = weigh_ins.sum(:total_calories).
-      where('team_id = ? and entry_date = ?', team.id, challenge_date)
-    total_calories = calories_from_meals - calories_from_exercise + calories_from_weigh_ins
-    (total_calories - target) / target
+  # Returns daily net actual calorie intake. Note: this method uses the
+  # 'groupdate' gem.
+  #
+  # @param team The team to compute statistics
+  #
+  # @returns A hash where keys correspond to the 30 day challenge dates, and
+  # values are the net meals, exercise and weigh-in adjustments, in calories.
+  def fetch_team_data(team)
+    # Fetch daily totals from meals, exercies and weigh-ins
+    meals_cals = meals.where('team_id = ?', team.id).
+                       group_by_day(:meal_date).sum(:total_calories)
+    exercise_cals = exercises.where('team_id = ?', team.id).
+                              group_by_day(:entry_date).sum(:total_calories)
+    weigh_in_cals = weigh_ins.where('team_id = ?', team.id).
+                              group_by_day(:entry_date).sum(:total_calories)
+
+    # Initialize a hash with 30 elements whose keys are dates beginning
+    # with the starting date
+    data = {}
+    (0..30).each do |i|
+      date = team.starting_on + i.days
+      data[date] = 0.00
+    end
+
+    # Assigns the net actual calorie instake to the corresponding key.
+    data.each do |date, value|
+      value = meals_cals[date] - exercise_cals[date] + weigh_in_cals[date]
+    end
+    data
   end
 end
