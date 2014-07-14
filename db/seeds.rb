46681bf397b53d156972cd8c61a91a08a2a3e5c1
Blair Caple <blair@kaaterskil.com>
2014-07-13 21:12:45 -0400

    Reseed

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,28 +3,55 @@ require 'factory_girl'
 User.delete_all
 Team.delete_all
 Roster.delete_all
+Ingredient.delete_all
+Meal.delete_all
+Exercise.delete_all
+WeighIn.delete_all
+Message.delete_all
 
-leader1 = FactoryGirl.create :user
-team1 = leader1.create_team(FactoryGirl.attributes_for(:team))
-user1 = FactoryGirl.create :user
-leader1.add_user_to_team(team1, user1)
-user2 = FactoryGirl.create :user
-leader1.add_user_to_team(team1, user2)
-user3 = FactoryGirl.create :user
-leader1.add_user_to_team(team1, user3)
+natasha = FactoryGirl.create :user,
+            email: 'natasha@example.com',
+            password: 'password',
+            password_confirmation: 'password',
+            known_by: 'Natasha',
+            gender: 'Female'
 
-leader2 = FactoryGirl.create :user
-team2 = leader2.create_team(FactoryGirl.attributes_for(:team))
-user4 = FactoryGirl.create :user
-leader2.add_user_to_team(team2, user4)
-user5 = FactoryGirl.create :user
-leader2.add_user_to_team(team2, user5)
-user6 = FactoryGirl.create :user
-leader2.add_user_to_team(team2, user6)
+marcus = FactoryGirl.create :user,
+            email: 'marcus@example.com',
+            password: 'password',
+            password_confirmation: 'password',
+            known_by: 'Marcus',
+            gender: 'Male'
 
-leader3 = FactoryGirl.create :user
-team3 = leader3.create_team(FactoryGirl.attributes_for(:team))
-user7 = FactoryGirl.create :user
-leader3.add_user_to_team(team3, user7)
-user8 = FactoryGirl.create :user
-leader3.add_user_to_team(team3, user8)
+alec = FactoryGirl.create :user,
+            email: 'alec@example.com',
+            password: 'password',
+            password_confirmation: 'password',
+            known_by: 'Alec',
+            gender: 'Female'
+
+jaye = FactoryGirl.create :user,
+            email: 'jaye@example.com',
+            password: 'password',
+            password_confirmation: 'password',
+            known_by: 'Jaye',
+            gender: 'Male'
+
+peter = FactoryGirl.create :user,
+            email: 'peter@example.com',
+            password: 'password',
+            password_confirmation: 'password',
+            known_by: 'Peter',
+            gender: 'Male'
+
+juliana = FactoryGirl.create :user,
+            email: 'juliana@example.com',
+            password: 'password',
+            password_confirmation: 'password',
+            known_by: 'Juliana',
+            gender: 'Female'
+
+tufsT1 = natasha.create_team(name: 'Tufts T1')
+
+natasha.add_user_to_team(tufsT1, juliana)
+natasha.add_user_to_team(tufsT1, alec)
